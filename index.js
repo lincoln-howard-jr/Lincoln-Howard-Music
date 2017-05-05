cc1c319496f06fa9d6e387ae017b92b789a341cf
lincoln-howard-jr <lchowardjr@gmail.com>
2017-05-05 13:51:43 -0400

    initial website version

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let express = require ('express');
+let fs = require ('fs');
+let html = require ('./pages'); 
+
+let pages = {
+  '/': html (fs.readFileSync ('index.html'))
+}
+
+let app = express ();
+
+app.use (require ('compression') ());
+app.get ('/', (req, res) => {
+  res.header ('X-Frame-Options', 'ALLOW-FROM https://www.youtube.com/');
+  res.header ('Content-Type', 'text/html');
+  res.send (pages ['/']);
+});
+app.get ('/static/:dir/:file', (req, res) => {
+  res.sendFile (require ('path').join (__dirname, 'static', req.params.dir, req.params.file));
+});
+// font awesome
+app.use ('/fa', express.static ('node_modules/font-awesome/css'));
+app.use ('/fonts', express.static ('node_modules/font-awesome/fonts'));
+
+app.listen (8080);
---
+++ pages.js
@@ -0,0 +1,76 @@
+module.exports = function (body) {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Experiments</title>
+      <base target="_blank">
+      <link rel="stylesheet" type="text/css" href="/static/css/bare.min.css">
+      <link rel="stylesheet" type="text/css" href="/fa/font-awesome.css">
+      <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+      <style>
+        * {
+          font-family: 'Nunito', sans-serif;
+        }
+        div.wrapper {
+          width: 100%;
+          height: 0;
+          padding-bottom: 56%;
+        }
+        iframe {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          display: block;
+        }
+        .sub-heading {
+          border-bottom: 1px solid #679;
+          height: 2rem;
+          margin-bottom: 4rem;
+        }
+        .sub-heading h2 {
+          position: absolute;
+          display: inline-block;
+          padding: 0 20px 0 0;
+          margin: 0;
+          background: #fff;
+          color: #679;
+        }
+        .sub-heading h2.right {
+          text-align: right;
+          right: 0;
+          padding: 0 0 0 20px;
+        }
+        .listen {
+          color: #111;
+          margin: 0 6px;
+        }
+      </style>
+    </head>
+    <body>
+
+      <nav>
+        <label>
+          <input type="checkbox">
+          <header>
+            <a href="/">Lincoln Howard Music</a>
+          </header>
+          <ul>
+            <li><a href="/upcoming">Upcoming</a></li>
+            <li><a href="/about">About</a></li>
+            <li>
+              <a href="#"></a>
+              <menu>
+                <menuitem></menuitem>
+              </menu>
+            </li>
+          </ul>
+        </label>
+      </nav>
+
+      ${body}
+
+    </body>
+    </html>
+  `
+};
